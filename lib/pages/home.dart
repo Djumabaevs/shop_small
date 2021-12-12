7e6eac8b50a541d1406f16ded70b84578855475e
BakytDzhumabaev <djumabaevb@gmail.com>
2021-12-12 10:16:05 +0600

    add icons to top app bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,7 +6,23 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        leading: BackButton(),
+        iconTheme: IconThemeData(color: Colors.black),
+        elevation: 0,
+        title: Text(
+          "Flutter demo",
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.notifications)),
+          IconButton(
+              onPressed: () {}, icon: Icon(Icons.shopping_cart_outlined)),
+        ],
+      ),
     );
   }
 }
